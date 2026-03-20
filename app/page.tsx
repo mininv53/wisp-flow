b90a610fc6bcf298d1af377f926f91d8c7e34b47
unknown <sholdershow23@gmail.com>
2026-03-20 18:07:46 +0200

    fix page imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,5 @@
-'use client'
-import AuthWrapper from '../components/AuthWrapper'
-import Flow from '../components/Flow'
+import LandingPage from './components/LandingPage'
 
-export default function FlowPage() {
-  return (
-    <AuthWrapper>
-      {(user, signOut) => (
-        <div>
-          <div className="fixed top-4 right-4 z-50 flex items-center gap-3">
-            <span className="text-gray-600 text-xs">{user?.email}</span>
-            <button onClick={signOut} className="text-gray-600 text-xs hover:text-white transition-colors">
-              Delogare
-            </button>
-          </div>
-          <Flow userId={user?.id} />
-        </div>
-      )}
-    </AuthWrapper>
-  )
+export default function Home() {
+  return <LandingPage />
 }
